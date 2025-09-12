3a81c63a0fd2bf84ea9236f3f31ec13ed127778b
Joe Mun <joe.mun@discordapp.com>
2025-09-11 17:13:08 -0700

    [discord] Reanimated patch related to RetryableMountingLayerException - https://github.com/discord/discord/commit/2188b1e812527ceaf75c52d1e95b86f22ea7b9d2

--- packages/react-native-reanimated/src/layoutReanimation/animationsManager.ts
+++ packages/react-native-reanimated/src/layoutReanimation/animationsManager.ts
@@ -1,23 +1,25 @@
-'use strict';
-import { withStyleAnimation } from '../animation/styleAnimation';
+"use strict";
+import { isAndroid } from "react-native-reanimated/src/PlatformChecker";
+import { withStyleAnimation } from "../animation/styleAnimation";
 import type {
   LayoutAnimation,
   LayoutAnimationStartFunction,
   SharedTransitionAnimationsValues,
   SharedValue,
-} from '../commonTypes';
-import { LayoutAnimationType } from '../commonTypes';
-import { makeMutableUI } from '../mutables';
-import { runOnUIImmediately } from '../threads';
+} from "../commonTypes";
+import { LayoutAnimationType } from "../commonTypes";
+import { makeMutableUI } from "../mutables";
+import { runOnUIImmediately } from "../threads";
 
 const TAG_OFFSET = 1e9;
+const IS_ANDROID = isAndroid();
 
 function startObservingProgress(
   tag: number,
   sharedValue: SharedValue<Record<string, unknown>>,
   animationType: LayoutAnimationType
 ): void {
-  'worklet';
+  "worklet";
   const isSharedTransition =
     animationType === LayoutAnimationType.SHARED_ELEMENT_TRANSITION;
   sharedValue.addListener(tag + TAG_OFFSET, () => {
@@ -30,7 +32,7 @@ function stopObservingProgress(
   sharedValue: SharedValue<number>,
   removeView = false
 ): void {
-  'worklet';
+  "worklet";
   sharedValue.removeListener(tag + TAG_OFFSET);
   global._notifyAboutEnd(tag, removeView);
 }
@@ -39,65 +41,76 @@ function createLayoutAnimationManager(): {
   start: LayoutAnimationStartFunction;
   stop: (tag: number) => void;
 } {
-  'worklet';
+  "worklet";
   const currentAnimationForTag = new Map();
   const mutableValuesForTag = new Map();
 
+  const startActually = (
+    tag: number,
+    type: LayoutAnimationType,
+    /**
+     * CreateLayoutAnimationManager creates an animation manager for both
+     * Layout animations and Shared Transition Elements animations.
+     */
+    yogaValues: Partial<SharedTransitionAnimationsValues>,
+    config: (arg: Partial<SharedTransitionAnimationsValues>) => LayoutAnimation
+  ) => {
+    if (type === LayoutAnimationType.SHARED_ELEMENT_TRANSITION_PROGRESS) {
+      global.ProgressTransitionRegister.onTransitionStart(tag, yogaValues);
+      return;
+    }
+
+    const style = config(yogaValues);
+    let currentAnimation = style.animations;
+
+    // When layout animation is requested, but a previous one is still running, we merge
+    // new layout animation targets into the ongoing animation
+    const previousAnimation = currentAnimationForTag.get(tag);
+    if (previousAnimation) {
+      currentAnimation = { ...previousAnimation, ...style.animations };
+    }
+    currentAnimationForTag.set(tag, currentAnimation);
+
+    let value = mutableValuesForTag.get(tag);
+    if (value === undefined) {
+      value = makeMutableUI(style.initialValues);
+      mutableValuesForTag.set(tag, value);
+    } else {
+      stopObservingProgress(tag, value);
+      value._value = style.initialValues;
+    }
+
+    // @ts-ignore The line below started failing because I added types to the method – don't have time to fix it right now
+    const animation = withStyleAnimation(currentAnimation);
+
+    animation.callback = (finished?: boolean) => {
+      if (finished) {
+        currentAnimationForTag.delete(tag);
+        mutableValuesForTag.delete(tag);
+        const shouldRemoveView = type === LayoutAnimationType.EXITING;
+        stopObservingProgress(tag, value, shouldRemoveView);
+      }
+      style.callback &&
+        style.callback(finished === undefined ? false : finished);
+    };
+
+    startObservingProgress(tag, value, type);
+    value.value = animation;
+  };
+
+  let start;
+  if (IS_ANDROID) {
+    // @ts-expect-error TODO: FIX ME
+    start = (tag, type, yogaValues, config) =>
+      requestAnimationFrame(() => {
+        startActually(tag, type, yogaValues, config);
+      });
+  } else {
+    start = startActually;
+  }
+
   return {
-    start(
-      tag: number,
-      type: LayoutAnimationType,
-      /**
-       * CreateLayoutAnimationManager creates an animation manager for both
-       * Layout animations and Shared Transition Elements animations.
-       */
-      yogaValues: Partial<SharedTransitionAnimationsValues>,
-      config: (
-        arg: Partial<SharedTransitionAnimationsValues>
-      ) => LayoutAnimation
-    ) {
-      if (type === LayoutAnimationType.SHARED_ELEMENT_TRANSITION_PROGRESS) {
-        global.ProgressTransitionRegister.onTransitionStart(tag, yogaValues);
-        return;
-      }
-
-      const style = config(yogaValues);
-      let currentAnimation = style.animations;
-
-      // When layout animation is requested, but a previous one is still running, we merge
-      // new layout animation targets into the ongoing animation
-      const previousAnimation = currentAnimationForTag.get(tag);
-      if (previousAnimation) {
-        currentAnimation = { ...previousAnimation, ...style.animations };
-      }
-      currentAnimationForTag.set(tag, currentAnimation);
-
-      let value = mutableValuesForTag.get(tag);
-      if (value === undefined) {
-        value = makeMutableUI(style.initialValues);
-        mutableValuesForTag.set(tag, value);
-      } else {
-        stopObservingProgress(tag, value);
-        value._value = style.initialValues;
-      }
-
-      // @ts-ignore The line below started failing because I added types to the method – don't have time to fix it right now
-      const animation = withStyleAnimation(currentAnimation);
-
-      animation.callback = (finished?: boolean) => {
-        if (finished) {
-          currentAnimationForTag.delete(tag);
-          mutableValuesForTag.delete(tag);
-          const shouldRemoveView = type === LayoutAnimationType.EXITING;
-          stopObservingProgress(tag, value, shouldRemoveView);
-        }
-        style.callback &&
-          style.callback(finished === undefined ? false : finished);
-      };
-
-      startObservingProgress(tag, value, type);
-      value.value = animation;
-    },
+    start,
     stop(tag: number) {
       const value = mutableValuesForTag.get(tag);
       if (!value) {
@@ -109,7 +122,7 @@ function createLayoutAnimationManager(): {
 }
 
 runOnUIImmediately(() => {
-  'worklet';
+  "worklet";
   global.LayoutAnimationsManager = createLayoutAnimationManager();
 })();
 
