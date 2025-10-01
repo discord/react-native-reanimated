d2903a3a414683ff1e0132c00530129c806362e8
Sebastian Kuśnierz <47144579+skusnierz@users.noreply.github.com>
2025-10-01 10:06:08 +0200

    (cherry-pick) Fix `Cannot read property '__nativeTag' of null`. (#8311) Cherry-pick of the https://github.com/software-mansion/react-native-reanimated/pull/8093
    ---------
    
    Co-authored-by: Phil Bazun <Phil9lne@gmail.com>

--- packages/react-native-reanimated/src/helperTypes.ts
+++ packages/react-native-reanimated/src/helperTypes.ts
@@ -7,7 +7,7 @@ until time comes to refactor the code and get necessary types right.
 This will not be easy though! 
 */
 
-import type { RegisteredStyle, StyleProp } from 'react-native';
+import type { StyleProp } from 'react-native';
 
 import type {
   AnimatedStyle,
@@ -20,7 +20,11 @@ import type { BaseAnimationBuilder } from './layoutReanimation/animationBuilder/
 import type { ReanimatedKeyframe } from './layoutReanimation/animationBuilder/Keyframe';
 import type { SharedTransition } from './layoutReanimation/sharedTransitions';
 
-type EntryOrExitLayoutType =
+// RegisteredStyle was removed from React Native, provide a fallback type
+// This is only used in a deprecated type definition
+type RegisteredStyle<T> = number & { __registeredStyleBrand: T };
+
+export type EntryOrExitLayoutType =
   | BaseAnimationBuilder
   | typeof BaseAnimationBuilder
   | EntryExitAnimationFunction
