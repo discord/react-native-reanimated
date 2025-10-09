1531a8cc63a9c3f5ad4bc66e09ca13763aa0dfeb
Sebastian Kuśnierz <47144579+skusnierz@users.noreply.github.com>
2025-10-09 09:58:33 +0200

    Run yarn build (#7)

--- packages/react-native-reanimated/lib/module/updateProps/ComponentRegistry.js
+++ packages/react-native-reanimated/lib/module/updateProps/ComponentRegistry.js
@@ -1,11 +1,20 @@
 "use strict";
 
-// Registry to map component tags to component instancesAdd commentMore actions
+// Registry to map component tags to component instances
 const componentRegistry = new Map();
+// Cache for delayed updates when components aren't found
+const pendingUpdates = new Map();
 export const ComponentRegistry = {
   // Register a component instance with its tag
   register: (tag, component) => {
     componentRegistry.set(tag, component);
+
+    // Check if there are pending updates for this tag
+    const pendingUpdate = pendingUpdates.get(tag);
+    if (pendingUpdate) {
+      component._updateReanimatedProps(pendingUpdate);
+      pendingUpdates.delete(tag);
+    }
   },
   // Unregister a component
   unregister: tag => {
@@ -14,6 +23,10 @@ export const ComponentRegistry = {
   // Get a component for a tag
   getComponent: tag => {
     return componentRegistry.get(tag);
+  },
+  // Cache an update for when component becomes available
+  cacheUpdate: (tag, props) => {
+    pendingUpdates.set(tag, props);
   }
 };
 //# sourceMappingURL=ComponentRegistry.js.map
