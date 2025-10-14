dd643d5694b31cdfd954c199e01f95f9924de2eb
sudoplz <sudoplz@gmail.com>
2025-10-14 19:12:53 +0300

    Revert "fix(reanimated): workaround for react-freeze unmounting class components on new arch (#6)"
    This reverts commit 10340faaf1d227f881cc061ab5b247cd77e696ea.

--- packages/react-native-reanimated/src/updateProps/ComponentRegistry.ts
+++ packages/react-native-reanimated/src/updateProps/ComponentRegistry.ts
@@ -1,21 +1,12 @@
 import type { IAnimatedComponentInternal } from '../createAnimatedComponent/commonTypes';
 
-// Registry to map component tags to component instances
+// Registry to map component tags to component instancesAdd commentMore actions
 const componentRegistry = new Map<number | HTMLElement, IAnimatedComponentInternal>();
-// Cache for delayed updates when components aren't found
-const pendingUpdates = new Map<number | HTMLElement, any>();
 
 export const ComponentRegistry = {
   // Register a component instance with its tag
   register: (tag: number | HTMLElement, component: IAnimatedComponentInternal) => {
     componentRegistry.set(tag, component);
-
-    // Check if there are pending updates for this tag
-    const pendingUpdate = pendingUpdates.get(tag);
-    if (pendingUpdate) {
-      component._updateReanimatedProps(pendingUpdate);
-      pendingUpdates.delete(tag);
-    }
   },
 
   // Unregister a component
@@ -26,10 +17,5 @@ export const ComponentRegistry = {
   // Get a component for a tag
   getComponent: (tag: number | HTMLElement): IAnimatedComponentInternal | undefined => {
     return componentRegistry.get(tag);
-  },
-
-  // Cache an update for when component becomes available
-  cacheUpdate: (tag: number | HTMLElement, props: any) => {
-    pendingUpdates.set(tag, props);
   }
 };
