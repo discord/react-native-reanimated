4a2c29c45a3671fca79c51b0dc04a64b016afe44
Hanno J. Gödecke <die.drei99@yahoo.de>
2025-10-15 19:16:33 +0200

    yarn build

--- packages/react-native-reanimated/lib/typescript/createAnimatedComponent/createAnimatedComponent.d.ts
+++ packages/react-native-reanimated/lib/typescript/createAnimatedComponent/createAnimatedComponent.d.ts
@@ -4,7 +4,15 @@ import type { FlatList, FlatListProps } from 'react-native';
 import type { AnimateProps } from '../helperTypes';
 import type { AnimatedComponentRef } from './commonTypes';
 type Options<P> = {
-    setNativeProps: (ref: AnimatedComponentRef, props: P) => void;
+  setNativeProps: (ref: AnimatedComponentRef, props: P) => void;
+  /**
+   * Discord enables a performance improvement, which causes us to sync back
+   * any animated props from the UI thread back to react JS. Switching this to
+   * `true` disables this behavior.
+   *
+   * @default false
+   */
+  disableReactSync?: boolean;
 };
 /**
  * Lets you create an Animated version of any React Native component.
@@ -13,13 +21,25 @@ type Options<P> = {
  * @returns A component that Reanimated is capable of animating.
  * @see https://docs.swmansion.com/react-native-reanimated/docs/core/createAnimatedComponent
  */
-export declare function createAnimatedComponent<P extends object>(component: FunctionComponent<P>, options?: Options<P>): FunctionComponent<AnimateProps<P>>;
-export declare function createAnimatedComponent<P extends object>(component: ComponentClass<P>, options?: Options<P>): ComponentClass<AnimateProps<P>>;
-export declare function createAnimatedComponent<P extends object>(component: ComponentType<P>, options?: Options<P>): FunctionComponent<AnimateProps<P>> | ComponentClass<AnimateProps<P>>;
+export declare function createAnimatedComponent<P extends object>(
+  component: FunctionComponent<P>,
+  options?: Options<P>
+): FunctionComponent<AnimateProps<P>>;
+export declare function createAnimatedComponent<P extends object>(
+  component: ComponentClass<P>,
+  options?: Options<P>
+): ComponentClass<AnimateProps<P>>;
+export declare function createAnimatedComponent<P extends object>(
+  component: ComponentType<P>,
+  options?: Options<P>
+): FunctionComponent<AnimateProps<P>> | ComponentClass<AnimateProps<P>>;
 /**
  * @deprecated Please use `Animated.FlatList` component instead of calling
  *   `Animated.createAnimatedComponent(FlatList)` manually.
  */
-export declare function createAnimatedComponent(component: typeof FlatList<unknown>, options?: Options<any>): ComponentClass<AnimateProps<FlatListProps<unknown>>>;
+export declare function createAnimatedComponent(
+  component: typeof FlatList<unknown>,
+  options?: Options<any>
+): ComponentClass<AnimateProps<FlatListProps<unknown>>>;
 export {};
 //# sourceMappingURL=createAnimatedComponent.d.ts.map
