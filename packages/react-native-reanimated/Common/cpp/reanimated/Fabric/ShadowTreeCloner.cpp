3cc7d707bc23bc48154ad9b78e0eaa75a188a081
Patrick Mick <patrick.mick@discordapp.com>
2025-11-19 12:23:39 -0800

    temp fix: access rawProps on android only

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.cpp
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ShadowTreeCloner.cpp
@@ -85,6 +85,7 @@ std::shared_ptr<ShadowNode> cloneShadowTreeWithNewPropsRecursive(
 
   Props::Shared newProps = mergeProps(shadowNode, propsMap, *family);
   
+#ifdef ANDROID
   if (newProps) {
       ReanimatedSystraceSection s("ShadowTreeCloner::equalityCheck");
 
@@ -97,6 +98,7 @@ std::shared_ptr<ShadowNode> cloneShadowTreeWithNewPropsRecursive(
           tagsToRemove.push_back(shadowNode.getTag());
       }
   }
+#endif
 
   return shadowNode.clone(
       {newProps ? newProps : ShadowNodeFragment::propsPlaceholder(),
