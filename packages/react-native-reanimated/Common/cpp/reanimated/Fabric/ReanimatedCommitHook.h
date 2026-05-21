ac368cb7665524533c8d1e85b49210fcf25a7dce
Krzysztof Piaskowy <krzysztof.piaskowy@discordapp.com>
2026-05-21 15:46:53 +0200

    Fork specific changes

--- packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.h
+++ packages/react-native-reanimated/Common/cpp/reanimated/Fabric/ReanimatedCommitHook.h
@@ -36,11 +36,11 @@ class ReanimatedCommitHook
       RootShadowNode::Unshared const &newRootShadowNode,
       const ShadowTreeCommitOptions& commitOptions) noexcept override;
 
-// Fork specific change, since this is used only in Discord's fork of react native.
-// void shadowTreeCommitSucceeded(const ShadowTreeCommitOptions& commitOptions) override;
-// void shadowTreeCommitFinalized(const ShadowTreeCommitOptions& commitOptions) override;
-  void shadowTreeCommitSucceeded(const ShadowTreeCommitOptions& commitOptions);
-  void shadowTreeCommitFinalized(const ShadowTreeCommitOptions& commitOptions);
+  // Note: If testing outside the Discord app, comment out these lines. The signature of these methods changed in Discord's fork of react native.
+  void shadowTreeCommitSucceeded(const ShadowTreeCommitOptions& commitOptions) override;
+  void shadowTreeCommitFinalized(const ShadowTreeCommitOptions& commitOptions) override;
+  // void shadowTreeCommitSucceeded(const ShadowTreeCommitOptions& commitOptions);
+  // void shadowTreeCommitFinalized(const ShadowTreeCommitOptions& commitOptions);
 
  private:
   std::shared_ptr<PropsRegistry> propsRegistry_;
